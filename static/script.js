7a2bf6d196e3b12f305734a6746e2c6005c4e592
Charles Kantz <kantz.charles@gmail.com>
2020-03-30 17:17:02 -0700

    HTML page with AJAX working

--- static/script.js
+++ static/script.js
@@ -1,35 +1,65 @@
-$(document).ready( function() {
+$(async function() {
 
-    async function getAllCupcakes(){
-        response = await axios.get('/api/cupcakes');
-        return response.data.cupcakes;
-    }
+  let $form = $("#new-cupcake-form");
+  let $flavorFlaves = $("#flavors");
+  let flavorList = [];
 
-    async function ListOfUsefulCupCakeData(){
-        cupcakes = await getAllCupcakes();
-        flavorList = [];
+  $form.on("submit", function(event){
+    event.preventDefault();
+    SendNewCupcake();
+  });
 
-        // for each JSONcupcake in AllCupcakes
-        // append to arr the flavor
-        for( let cake in cupcakes){
-            flavorList.push(cake.flavor)
-        }
-        return flavorList
-    }
+  async function SendNewCupcake(){
+    
+    let flavor = $("#form-flavor").val();
+    let size = $("#form-size").val();
+    let rating = $("#form-rating").val();
+    let image = $("#form-image").val();
+    
+    let response = await axios.post('/api/cupcakes', data={
+      flavor,
+      size,
+      rating,
+      image
+    });
 
-    function addFlavorsToHTML(flavorList){
-        // for loop of flavors
-            // create <li> with .text of "flavor"
-            // add to list
-        $flavorFlaves = $("#flavors");
-        flavorList.forEach(element => {
-            let $flavor = $("<li>")
-                        .text(element);
-            $flavorFlaves.append($flavor);
-        });
-    }
-
-    flavorlist = ListOfUsefulCupCakeData();
+    flavorList.push(flavor);
     addFlavorsToHTML(flavorList);
+
+    $( '#new-cupcake-form' ).trigger("reset");
+  };
+  
+
+  async function getAllCupcakes(){
+      response = await axios.get('/api/cupcakes');
+      return response.data.cupcakes;
+  }
+
+  async function ListOfUsefulCupCakeData(){
+      cupcakes = await getAllCupcakes();
+
+
+      // for each JSONcupcake in AllCupcakes
+      // append to arr the flavor
+      for( let cake of cupcakes){
+          flavorList.push(cake.flavor)
+      }
+      return flavorList
+  }
+
+  function addFlavorsToHTML(flavorList){
+      // for loop of flavors
+          // create <li> with .text of "flavor"
+          // add to list
+      $flavorFlaves.empty();
+      for ( let flav of flavorList){
+          let $flavor = $("<li>")
+                      .text(flav);
+          $flavorFlaves.append($flavor);
+      };
+  }
+
+  flavorList = await ListOfUsefulCupCakeData();
+  addFlavorsToHTML(flavorList);
 
 });
